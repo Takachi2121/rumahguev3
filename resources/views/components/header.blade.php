cf16c1c92385836855551751a0b1a6136b29efd3
andremm73 <andremm73@gmail.com.>
2025-12-11 10:53:36 +0700

    First Commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<div class="container mt-4 px-0">
+    <nav class="navbar-glass d-flex justify-content-between align-items-center">
+
+        <!-- LEFT LOGO -->
+        <div class="navbar-logo">
+            <img src="{{ asset('assets/img/logo.png') }}" alt="Logo">
+        </div>
+
+        <!-- CENTER MENU -->
+        <div>
+            <a href="#" class="text-decoration-none">Beranda</a>
+            <a href="#" class="text-decoration-none">Tentang Kami</a>
+            <a href="#" class="text-decoration-none">Kategori</a>
+        </div>
+
+        <!-- RIGHT BUTTON -->
+        <button class="btn-signin">Sign In</button>
+    </nav>
+</div>
---
+++ resources/views/parts/home.blade.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+
+<div class="container mt-3 px-0">
+    <div class="home-banner d-flex flex-column justify-content-center align-items-center text-center">
+        <p class="home-title mb-0 mt-5">Bangun Rumah Loe Sekarang!</p>
+        <p class="home-subtitle mb-4">Wujudkan rumah impianmu dengan mudah bersama layanan profesional kami dari desain, konstruksi, hingga selesai.</p>
+        <button class="btn-home mb-5">Coba Sekarang</button>
+
+        <div class="home-content">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="home-content-item">
+                        <img src="{{ asset('assets/img/LandingPage/icon-design.png') }}" alt="Design Icon" class="mb-3">
+                        <h5>Desain Profesional</h5>
+                        <p>Dapatkan desain rumah yang sesuai dengan gaya hidup dan kebutuhan Anda.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
