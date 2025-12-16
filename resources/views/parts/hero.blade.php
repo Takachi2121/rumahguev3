4ad4a34af5247bb3312f2ee230c8127311219d64
andremm73 <andremm73@gmail.com.>
2025-12-16 14:53:14 +0700

    Ringkasan testimoni di sebelah kanan bawah

--- resources/views/parts/hero.blade.php
+++ resources/views/parts/hero.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/hero.css') }}">
 
 <div class="container mt-3 px-0">
     <div class="home-banner d-flex flex-column justify-content-center align-items-center text-center">
@@ -6,9 +6,9 @@
         <p class="home-subtitle mb-4">Wujudkan rumah impianmu dengan mudah bersama layanan profesional kami dari desain, konstruksi, hingga selesai.</p>
         <button class="btn-home mb-5">Coba Sekarang</button>
 
-        <div class="home-content" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200">
-            <div class="row home-content-row">
-                <div class="d-flex flex-column home-card-row" data-aos="zoom-in" data-aos-duration="500" data-aos-delay="300">
+        <div class="row home-content" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200" data-aos-once="true">
+            <div class="row home-content-row" data-aos="zoom-in" data-aos-duration="500" data-aos-delay="300" data-aos-once="true">
+                <div class="col-md-12 d-flex flex-column home-card-row">
                     <div class="home-card-title">
                         <div class="d-flex gap-2 align-items-center">
                             <img src="{{ asset('assets/img/Vector/Treeview.png') }}" width="15" alt="Icon Treeview">
@@ -34,6 +34,29 @@
                             <p class="fw-medium mb-2 fs-5">Jasa Arsitek</p>
                             <p class="btn-contact">Hubungi Kami</p>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <!-- RIGHT BOTTOM STATS -->
+            <div class="home-stats">
+                <div class="stats-card">
+                    <div class="stats-left">
+                        <div class="stats-avatars">
+                            <img src="{{ asset('assets/img/Person/Girl.png') }}">
+                            <img src="{{ asset('assets/img/Person/Man.png') }}">
+                            <img src="{{ asset('assets/img/Person/Boy.png') }}">
+                        </div>
+                        <div class="stats-text">
+                            <p class="stats-number text-start">1000+ Orang</p>
+                            <p class="stats-desc text-start">Berhasil membangun rumah impian mereka</p>
+                        </div>
+                    </div>
+
+                    <div class="stats-divider"></div>
+
+                    <div class="stats-right">
+                        <p class="stats-number text-start">2.500+ orang</p>
+                        <p class="stats-desc text-start">Memberikan ulasan sangat baik</p>
                     </div>
                 </div>
             </div>
