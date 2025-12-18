367a15f98259dda0093eb4d96eed1ede50d4a865
takachi2121 <ntakachi73@gmail.com>
2025-12-18 14:32:59 +0700

    link css

--- resources/views/parts/testimoni.blade.php
+++ resources/views/parts/testimoni.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="{{ asset('assets/css/testimoni.css') }}">
+
 <div class="container my-3">
     <p class="py-2 px-4 fw-semibold label-title bg-transparent">Testimoni</p>
 </div>
