234e1f42febb64467ab6a1dda8167973181d7d94
Andre <andremm73@gmail.com>
2026-02-13 09:19:47 +0700

    Perbaikan

--- resources/views/parts/kota.blade.php
+++ resources/views/parts/kota.blade.php
@@ -1,130 +1,69 @@
-<!-- Aceh -->
 <option value="Aceh Besar">Aceh Besar</option>
 <option value="Banda Aceh">Banda Aceh</option>
-<option value="Langsa">Langsa</option>
-
-<!-- Sumatera Utara -->
-<option value="Medan">Medan</option>
-<option value="Binjai">Binjai</option>
-<option value="Pematangsiantar">Pematangsiantar</option>
-
-<!-- Sumatera Barat -->
-<option value="Padang">Padang</option>
-<option value="Bukittinggi">Bukittinggi</option>
-
-<!-- Riau -->
-<option value="Pekanbaru">Pekanbaru</option>
-
-<!-- Jambi -->
-<option value="Jambi">Jambi</option>
-
-<!-- Sumatera Selatan -->
-<option value="Palembang">Palembang</option>
-
-<!-- Bengkulu -->
-<option value="Bengkulu">Bengkulu</option>
-
-<!-- Lampung -->
 <option value="Bandar Lampung">Bandar Lampung</option>
-
-<!-- Banten -->
-<option value="Serang">Serang</option>
-<option value="Tangerang">Tangerang</option>
-
-<!-- DKI Jakarta -->
+<option value="Bandung">Bandung</option>
+<option value="Banyuwangi">Banyuwangi</option>
+<option value="Batu">Batu</option>
+<option value="Bekasi">Bekasi</option>
+<option value="Bengkulu">Bengkulu</option>
+<option value="Biak">Biak</option>
+<option value="Bitung">Bitung</option>
+<option value="Blitar">Blitar</option>
+<option value="Bontang">Bontang</option>
+<option value="Bukittinggi">Bukittinggi</option>
+<option value="Ciamis">Ciamis</option>
+<option value="Cianjur">Cianjur</option>
+<option value="Cimahi">Cimahi</option>
+<option value="Cirebon">Cirebon</option>
+<option value="Cilacap">Cilacap</option>
+<option value="Denpasar">Denpasar</option>
+<option value="Depok">Depok</option>
+<option value="Gorontalo">Gorontalo</option>
+<option value="Indramayu">Indramayu</option>
+<option value="Jakarta Barat">Jakarta Barat</option>
 <option value="Jakarta Pusat">Jakarta Pusat</option>
 <option value="Jakarta Selatan">Jakarta Selatan</option>
-<option value="Jakarta Barat">Jakarta Barat</option>
 <option value="Jakarta Timur">Jakarta Timur</option>
 <option value="Jakarta Utara">Jakarta Utara</option>
-
-<!-- Jawa Barat -->
-<option value="Bandung">Bandung</option>
-<option value="Bekasi">Bekasi</option>
-<option value="Bogor">Bogor</option>
-<option value="Depok">Depok</option>
-<option value="Cirebon">Cirebon</option>
-<option value="Sukabumi">Sukabumi</option>
-<option value="Tasikmalaya">Tasikmalaya</option>
-<option value="Cimahi">Cimahi</option>
-<option value="Karawang">Karawang</option>
-<option value="Cianjur">Cianjur</option>
-<option value="Ciamis">Ciamis</option>
-
-<!-- Jawa Tengah -->
-<option value="Semarang">Semarang</option>
-<option value="Solo (Surakarta)">Solo (Surakarta)</option>
-<option value="Magelang">Magelang</option>
-<option value="Salatiga">Salatiga</option>
-<option value="Pekalongan">Pekalongan</option>
-<option value="Tegal">Tegal</option>
-<option value="Purwokerto">Purwokerto</option>
-<option value="Cilacap">Cilacap</option>
-<option value="Jepara">Jepara</option>
-
-<!-- DI Yogyakarta -->
-<option value="Yogyakarta">Yogyakarta</option>
-
-<!-- Jawa Timur -->
-<option value="Surabaya">Surabaya</option>
-<option value="Malang">Malang</option>
-<option value="Kediri">Kediri</option>
-<option value="Blitar">Blitar</option>
-<option value="Madiun">Madiun</option>
-<option value="Mojokerto">Mojokerto</option>
-<option value="Pasuruan">Pasuruan</option>
-<option value="Probolinggo">Probolinggo</option>
-<option value="Batu">Batu</option>
-<option value="Banyuwangi">Banyuwangi</option>
+<option value="Jambi">Jambi</option>
 <option value="Jember">Jember</option>
-<option value="Ponorogo">Ponorogo</option>
-
-<!-- Bali -->
-<option value="Denpasar">Denpasar</option>
-
-<!-- Nusa Tenggara Barat -->
-<option value="Mataram">Mataram</option>
-
-<!-- Nusa Tenggara Timur -->
-<option value="Kupang">Kupang</option>
-
-<!-- Kalimantan Barat -->
-<option value="Pontianak">Pontianak</option>
-<option value="Singkawang">Singkawang</option>
-
-<!-- Kalimantan Tengah -->
-<option value="Palangkaraya">Palangkaraya</option>
-
-<!-- Kalimantan Selatan -->
-<option value="Banjarmasin">Banjarmasin</option>
-
-<!-- Kalimantan Timur -->
-<option value="Balikpapan">Balikpapan</option>
-<option value="Samarinda">Samarinda</option>
-<option value="Tarakan">Tarakan</option>
-<option value="Bontang">Bontang</option>
-
-<!-- Sulawesi Utara -->
-<option value="Manado">Manado</option>
-<option value="Bitung">Bitung</option>
-
-<!-- Sulawesi Tengah -->
-<option value="Palu">Palu</option>
-
-<!-- Sulawesi Selatan -->
-<option value="Makassar">Makassar</option>
-<option value="Parepare">Parepare</option>
-
-<!-- Sulawesi Tenggara -->
+<option value="Jepara">Jepara</option>
+<option value="Karawang">Karawang</option>
+<option value="Kediri">Kediri</option>
 <option value="Kendari">Kendari</option>
-
-<!-- Gorontalo -->
-<option value="Gorontalo">Gorontalo</option>
-
-<!-- Papua -->
-<option value="Jayapura">Jayapura</option>
-<option value="Sorong">Sorong</option>
-<option value="Biak">Biak</option>
-<option value="Timika">Timika</option>
-<option value="Merauke">Merauke</option>
+<option value="Langsa">Langsa</option>
+<option value="Lumajang">Lumajang</option>
+<option value="Magelang">Magelang</option>
+<option value="Malang">Malang</option>
+<option value="Manado">Manado</option>
+<option value="Madiun">Madiun</option>
+<option value="Majalengka">Majalengka</option>
+<option value="Metro">Metro</option>
+<option value="Mojokerto">Mojokerto</option>
+<option value="Mataram">Mataram</option>
+<option value="Minahasa">Minahasa</option>
 <option value="Nabire">Nabire</option>
+<option value="Palangkaraya">Palangkaraya</option>
+<option value="Padang">Padang</option>
+<option value="Palembang">Palembang</option>
+<option value="Palu">Palu</option>
+<option value="Pangandaran">Pangandaran</option>
+<option value="Pekalongan">Pekalongan</option>
+<option value="Pekanbaru">Pekanbaru</option>
+<option value="Ponorogo">Ponorogo</option>
+<option value="Probolinggo">Probolinggo</option>
+<option value="Purwokerto">Purwokerto</option>
+<option value="Salatiga">Salatiga</option>
+<option value="Samarinda">Samarinda</option>
+<option value="Serang">Serang</option>
+<option value="Situbondo">Situbondo</option>
+<option value="Solo (Surakarta)">Solo (Surakarta)</option>
+<option value="Subang">Subang</option>
+<option value="Surabaya">Surabaya</option>
+<option value="Tasikmalaya">Tasikmalaya</option>
+<option value="Tangerang">Tangerang</option>
+<option value="Tegal">Tegal</option>
+<option value="Tarakan">Tarakan</option>
+<option value="Timika">Timika</option>
+<option value="Wamena">Wamena</option>
+<option value="Yogyakarta">Yogyakarta</option>
