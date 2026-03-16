06972a41967be295e60a77ca852a7821fa31ed02
Andre <andremm73@gmail.com>
2026-03-16 15:32:06 +0700

    error

--- public/assets/js/simulasi/print.js
+++ public/assets/js/simulasi/print.js
@@ -168,7 +168,7 @@ function sendRAB() {
                         showProgressBar: true,
                     });
 
-                    console.log(error.response.data);
+                    console.log(error.response);
                 });
         });
     }
